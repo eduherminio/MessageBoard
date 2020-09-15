791af149145e8316715a118e87af7dc9c678f0e2
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-09-15 20:20:10 +0200

    Add persistance using EF Core and in memory Db

---
+++ src/MessageBoard/MessageBoardDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MessageBoard
+{
+    public class MessageBoardDbContext : DbContext
+    {
+        // TODO add models
+
+        public MessageBoardDbContext(DbContextOptions<MessageBoardDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
